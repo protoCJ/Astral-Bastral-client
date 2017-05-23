7039f42a83aa16ee02218197d4811604985d3b4d
pawelkopec <pawelkopec10@gmail.com>
2017-05-23 14:51:22 +0200

    KeyInputHandler: add switch to keyPressed()

--- src/game/input/KeyInputHandler.java
+++ src/game/input/KeyInputHandler.java
@@ -10,13 +10,7 @@ public class KeyInputHandler extends KeyAdapter {
     /** The number of key presses we've had while waiting for an "any key" press */
     private int pressCount = 1;
 
-    private boolean waitingForKeyPress = true;
-    /** True if the left cursor key is currently pressed */
-    private boolean leftPressed = false;
-    /** True if the right cursor key is currently pressed */
-    private boolean rightPressed = false;
-    /** True if we are firing */
-    private boolean firePressed = false;
+    private boolean rotating = true;
 
     /**
      * Notification from AWT that a key has been pressed. Note that
@@ -26,21 +20,14 @@ public class KeyInputHandler extends KeyAdapter {
      * @param e The details of the key that was pressed
      */
     public void keyPressed(KeyEvent e) {
-        // if we're waiting for an "any key" typed then we don't
-        // want to do anything with just a "press"
-        if (waitingForKeyPress) {
-            return;
-        }
-
-
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            leftPressed = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            rightPressed = true;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            firePressed = true;
+        //TODO
+        switch (e.getKeyCode())  {
+            case KeyEvent.VK_LEFT:
+                break;
+            case KeyEvent.VK_RIGHT:
+                break;
+            case KeyEvent.VK_SPACE:
+                break;
         }
     }
 
@@ -50,21 +37,6 @@ public class KeyInputHandler extends KeyAdapter {
      * @param e The details of the key that was released
      */
     public void keyReleased(KeyEvent e) {
-        // if we're waiting for an "any key" typed then we don't
-        // want to do anything with just a "released"
-        if (waitingForKeyPress) {
-            return;
-        }
-
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            leftPressed = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            rightPressed = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            firePressed = false;
-        }
     }
 
     /**
@@ -74,29 +46,6 @@ public class KeyInputHandler extends KeyAdapter {
      * @param e The details of the key that was typed.
      */
     public void keyTyped(KeyEvent e) {
-        // if we're waiting for a "any key" type then
-        // check if we've recieved any recently. We may
-        // have had a keyType() event from the user releasing
-        // the shoot or move keys, hence the use of the "pressCount"
-        // counter.
-
-        if (waitingForKeyPress) {
-            if (pressCount == 1) {
-                // since we've now recieved our key typed
-
-                // event we can mark it as such and start
-
-                // our new game
-
-                waitingForKeyPress = false;
-                pressCount = 0;
-            } else {
-                pressCount++;
-            }
-        }
-
-        // if we hit escape, then quit the game
-
         if (e.getKeyChar() == 27) {
             System.exit(0);
         }
