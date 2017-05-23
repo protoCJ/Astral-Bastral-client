f32992c2b294fbe3d9dbf87f2b194920b74ed0db
pawelkopec <pawelkopec10@gmail.com>
2017-05-23 14:20:26 +0200

    Move SchedulerThread to separate file

--- src/game/input/ActionHandler.java
+++ src/game/input/ActionHandler.java
@@ -7,9 +7,6 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-
-import static java.util.concurrent.TimeUnit.*;
 
 /**
  * Created by protoCJ on 23.04.2017.
@@ -31,7 +28,7 @@ public class ActionHandler implements Runnable {
     }
 
     public void start() {
-        (new SchedulerThread(this, scheduler)).start();
+        new Thread(new SchedulerThread(this, scheduler)).start();
     }
 
     @Override
@@ -45,25 +42,5 @@ public class ActionHandler implements Runnable {
 
     byte[] prepareData() {
         return ByteBuffer.allocate(4).putFloat(7.0f/*game.getPlayerRotation()*/).array();
-    }
-}
-
-
-class SchedulerThread extends Thread {
-
-    Runnable toSchedule;
-    ScheduledExecutorService scheduler;
-
-    public void run() {
-        final ScheduledFuture<?> scheduledFuture =
-                scheduler.scheduleAtFixedRate(toSchedule, 0, 300, MILLISECONDS);
-        scheduler.schedule(new Runnable() {
-            public void run() { scheduledFuture.cancel(true); }
-        }, 60 * 60, SECONDS);
-    }
-
-    public SchedulerThread(Runnable toSchedule, ScheduledExecutorService scheduler) {
-        this.toSchedule = toSchedule;
-        this.scheduler = scheduler;
     }
 }
---
+++ src/game/input/SchedulerThread.java
@@ -0,0 +1,32 @@
+package game.input;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * Created by vivace on 23.05.17.
+ */
+
+
+
+class SchedulerThread implements Runnable {
+
+    Runnable toSchedule;
+    ScheduledExecutorService scheduler;
+
+    public SchedulerThread(Runnable toSchedule, ScheduledExecutorService scheduler) {
+        this.toSchedule = toSchedule;
+        this.scheduler = scheduler;
+    }
+
+    public void run() {
+        final ScheduledFuture<?> scheduledFuture =
+                scheduler.scheduleAtFixedRate(toSchedule, 0, 300, MILLISECONDS);
+        scheduler.schedule(new Runnable() {
+            public void run() { scheduledFuture.cancel(true); }
+        }, 60 * 60, SECONDS);
+    }
+}
